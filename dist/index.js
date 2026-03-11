79f993a3a42a840865b04499936d85c1590bbb05
Teodor <stevanovicteodor002@gmail.com>
2026-03-11 13:41:51 +0100

    feat: add average calculation and optimize number conversion

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
 "use strict";
 let numbers = ["10", "5", "20", "8", "3"];
+let numbersConverted = [];
 let sum = 0;
 for (let i = 0; i < numbers.length; i++) {
-    sum += Number(numbers[i]);
+    let number = Number(numbers[i]);
+    numbersConverted.push(number);
+    sum += number;
 }
 ;
+let average = sum / numbersConverted.length;
 console.log(numbers);
-console.log(sum);
+console.log("Numbers: ", numbersConverted);
+console.log("Sum: ", sum);
+console.log("Average: ", average);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 let numbers: string[] = ["10", "5", "20", "8", "3"];
+let numbersConverted: number[] = [];
 
 let sum: number = 0;
 
+
 for (let i = 0; i < numbers.length; i++) {
-    sum += Number(numbers[i]);
+    let number = Number(numbers[i])
+    numbersConverted.push(number);
+    sum += number;
 };
 
+let average: number = sum / numbersConverted.length;
+
 console.log(numbers);
-console.log(sum);
+console.log("Numbers: ", numbersConverted);
+console.log("Sum: ", sum);
+console.log("Average: ", average);
