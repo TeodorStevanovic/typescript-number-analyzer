06255aa9931f52e23773d3ff10d0d45d28167f83
Teodor <stevanovicteodor002@gmail.com>
2026-03-11 13:20:58 +0100

    feat: convert string numbers to number array

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,10 @@
 "use strict";
 let numbers = ["10", "5", "20", "8", "3"];
+let numberConverted = [];
+for (let i = 0; i < numbers.length; i++) {
+    numberConverted.push(Number(numbers[i]));
+}
+;
 console.log(numbers);
+console.log(numberConverted);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 let numbers = ["10", "5", "20", "8", "3"];
+let numberConverted = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    numberConverted.push(Number(numbers[i]))
+};
 
 console.log(numbers);
+console.log(numberConverted);
