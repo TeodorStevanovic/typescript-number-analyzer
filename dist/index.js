a60d0d11ba150b4fa59e7ba0e79ab2d97585ca41
Teodor <stevanovicteodor002@gmail.com>
2026-03-11 15:50:23 +0100

    feat: implement max number calculation

--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,14 @@
 let numbers = ["10", "5", "20", "8", "3"];
 let numbersConverted = [];
 let sum = 0;
+let max = Number(numbers[0]);
 for (let i = 0; i < numbers.length; i++) {
     let number = Number(numbers[i]);
     numbersConverted.push(number);
     sum += number;
+    if (number > max) {
+        max = number;
+    }
 }
 ;
 let average = sum / numbersConverted.length;
@@ -13,4 +17,5 @@ console.log(numbers);
 console.log("Numbers: ", numbersConverted);
 console.log("Sum: ", sum);
 console.log("Average: ", average);
+console.log("Max Number: ", max);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ let numbers: string[] = ["10", "5", "20", "8", "3"];
 let numbersConverted: number[] = [];
 
 let sum: number = 0;
+let max: number = Number(numbers[0]);
 
 
 for (let i = 0; i < numbers.length; i++) {
-    let number = Number(numbers[i])
+    let number = Number(numbers[i]);
     numbersConverted.push(number);
     sum += number;
+
+    if (number > max) {
+        max = number
+    }
 };
 
 let average: number = sum / numbersConverted.length;
@@ -16,3 +21,4 @@ console.log(numbers);
 console.log("Numbers: ", numbersConverted);
 console.log("Sum: ", sum);
 console.log("Average: ", average);
+console.log("Max Number: ", max);
